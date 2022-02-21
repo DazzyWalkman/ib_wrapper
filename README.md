869c1ecff389a5f14b53ed48387ec9d10a55c323
DazzyWalkman <20811819+DazzyWalkman@users.noreply.github.com>
2022-02-21 15:12:26 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+This is a wrapper script for the OpenWrt image builder. It will download the respective image builder and build the firmware image according to the user-specified image profile.
+
+Prerequisites: Same as the OpenWrt image builder.
+
+Usage:
+execute the script in its directory:
+./build.sh <path and filename of the image profile> <action>
+
+action type:
+image: build the firmware image according to the user-specified image profile.
+clean: remove the respective image builder files in the working directory acoording to the user-specified image profile.
+
+Example:
+build a firmware for profile 'profile_example': ./build.sh profile/profile_example image
+The resulting image file will be placed in the bin directory.
+
+remove a device-related image builder files: ./build.sh profile/profile_example clean
+This will remove the image builder files for the subtarget defined in the profile 'profile_example' in the working directory.
+
+Configuration:
+Please refer to conf/main.conf.example
+
+Image profile:
+Please refer to profile/profile_example
+
+See also:
+https://openwrt.org/docs/guide-user/additional-software/imagebuilder
---
+++ build.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+usage() {
+    echo "$0 profile <image|clean>"
+    exit 1
+}
+init_var() {
+    profile=$1
+    type=""
+    ver=""
+    confile="conf/main.conf"
+    mirror="downloads.openwrt.org"
+    if [ ! -e "$profile" ]; then
+        echo "Image profile not found. Abort."
+        exit 1
+    fi
+    target=$(grep "^TARGET" "$profile" | cut -d"=" -f2)
+    subtarget=$(grep "^SUBTARGET" "$profile" | cut -d"=" -f2)
+    devname=$(grep "^PROFILE" "$profile" | cut -d"=" -f2)
+    type=$(grep "^TYPE" "$profile" | cut -d"=" -f2)
+    packages=$(grep "^PACKAGES" "$profile" | cut -d"=" -f2)
+    files=$(grep "^FILES" "$profile" | cut -d"=" -f2)
+    dsvcs=$(grep "^DISABLED_SERVICES" "$profile" | cut -d"=" -f2)
+    if [ "$type" = "releases" ]; then
+        ver=$(grep "^VERSION" "$profile" | cut -d"=" -f2)
+        if [ -z "$ver" ]; then
+            echo "Missing releases version in the image profile. Abort."
+        fi
+    fi
+    if [ -z "$target" ] || [ -z "$subtarget" ] || [ -z "$devname" ] || [ -z "$type" ] || { [ "$type" != "snapshots" ] && [ "$type" != "releases" ]; }; then
+        echo "Invalid image profile. Abort."
+        exit 1
+    fi
+
+    if [ -e "$confile" ]; then
+        local mirror_tmp
+        mirror_tmp=$(grep "^MIRROR" "$confile" | cut -d"=" -f2)
+        if [ -n "$mirror_tmp" ]; then
+            mirror="$mirror_tmp"
+        else
+            echo "Invalid conf file. Abort."
+            exit 1
+        fi
+    fi
+    if [ "$type" = "releases" ]; then
+        url=https://"$mirror"/"$type"/"$ver"/targets/"$target"/"$subtarget"
+        ib_dir=openwrt-imagebuilder-"$ver"-"$target"-"$subtarget".Linux-x86_64
+
+    else
+        url=https://"$mirror"/"$type"/targets/"$target"/"$subtarget"
+        ib_dir=openwrt-imagebuilder-"$target"-"$subtarget".Linux-x86_64
+    fi
+    ib_name="$ib_dir".tar.xz
+}
+
+cleanup() {
+    rm -rf working/"$ib_dir" || exit 1
+    exit 0
+}
+
+build() {
+    if [ ! -d working/"$ib_dir" ]; then
+        mkdir -p working
+        if wget "$url"/"$ib_name" -P working/; then
+            tar xf working/"$ib_name" -C working/ || exit 1
+            rm working/"$ib_name"
+        fi
+    fi
+    mkdir -p bin
+    cd working/"$ib_dir" || exit 1
+    if [ ! -d ./bin ]; then
+        ln -s ../../bin bin
+    fi
+    sed -i 's,downloads.openwrt.org,'"$mirror"',g' repositories.conf
+    rev=$(make info | grep "Current Revision" | cut -d"\"" -f2)
+    if [ -z "$rev" ]; then
+        echo "Invalid Image Builder."
+        exit 1
+    else
+        make image PROFILE="$devname" PACKAGES="$packages" FILES="$files" DISABLED_SERVICES="$dsvcs" EXTRA_IMAGE_NAME="$rev"
+        exit 0
+    fi
+}
+if [ -z "$2" ] || [ -n "$3" ]; then
+    usage
+fi
+init_var "$@"
+subcmd="$2"
+case $subcmd in
+    "clean") cleanup ;;
+    "image") build ;;
+    *) usage ;;
+esac
